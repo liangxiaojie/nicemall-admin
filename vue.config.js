1b5eed177cd1600a28ec23cd1632aa156a3db32b
liangxiaojie <15201515601@163.com>
2018-05-28 21:38:16 +0800

    svg-icon

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
+const path = require('path');
+
 module.exports = {
   lintOnSave: false,
+  chainWebpack: (config) => {
+    config.module
+      .rule('svg')
+      .test(/\.(svg)(\?.*)?$/)
+      .include.add(path.join(__dirname, 'src/icons')).end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]',
+      });
+  },
 };
