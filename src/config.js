25cc7205c4290d49964b90c2144f0af37302a9fe
liangxiaojie <15201515601@163.com>
2018-06-06 00:51:18 +0800

    fixed env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL || 'http://localhost:7001';
+export const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:7001';
 
 export default {
   API_URL,
